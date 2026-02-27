244929e55e99c1496f5dab1b5780e7c8720a6ff3
Shift7 <luky.hubert@gmail.com>
2026-02-27 20:28:53 +0100

    Added tilemap

--- Scrpits/player.gd
+++ Scrpits/player.gd
@@ -1,23 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 30.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 3.0
+const JUMP_VELOCITY = -100.0
+const SLIPPERY_INDEX = 1
 
 @onready var sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta * 0.1
+		velocity += get_gravity() * delta * 0.2
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump"):# and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x += direction * SPEED
+	elif velocity.x > 0:
+		velocity.x -= SLIPPERY_INDEX
 	
 	if direction != 0:
 		sprite.play("walk")
