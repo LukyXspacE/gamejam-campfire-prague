5c2589aa6ca5c469332b172c352285a149d778ae
Shift7 <luky.hubert@gmail.com>
2026-02-27 22:47:07 +0100

    Adding cursor for cintroler

--- Scrpits/map.gd
+++ Scrpits/map.gd
@@ -14,6 +14,7 @@ func _input(event: InputEvent) -> void:
 		erase_cell(cell)                 # remove the tile
 	
 	if event.is_action_pressed("Build"):
-		var mouse_pos = get_local_mouse_position()       # mouse relative to TileMap
-		var cell = local_to_map(mouse_pos)              # convert to tile coordinates
-		#set_cell()
+		var mouse_pos = get_local_mouse_position()
+		var cell = local_to_map(mouse_pos)
+		if get_cell_source_id(cell) == -1:
+			set_cell(cell, 1, Vector2i(0,0))
