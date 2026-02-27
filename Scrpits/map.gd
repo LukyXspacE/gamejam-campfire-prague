005d427eeae2ddb96369df08815c14cff96151c0
Shift7 <luky.hubert@gmail.com>
2026-02-27 21:21:33 +0100

    Added structures tileset and breaking mechanic

--- Scrpits/map.gd
+++ Scrpits/map.gd
@@ -6,6 +6,14 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	
+	if event.is_action_pressed("Mine"):
+		var mouse_pos = get_local_mouse_position()       # mouse relative to TileMap
+		var cell = local_to_map(mouse_pos)              # convert to tile coordinates
+		erase_cell(cell)                 # remove the tile
+	
+	if event.is_action_pressed("Build"):
+		var mouse_pos = get_local_mouse_position()       # mouse relative to TileMap
+		var cell = local_to_map(mouse_pos)              # convert to tile coordinates
+		#set_cell()
